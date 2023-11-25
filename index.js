d7d6ccfb2372d243072b2e52bdfe686951058f25
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2023-11-25 11:03:43 -0600

    *tutoring session* changed questions array into a class and added other classes

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const inquirer = require('inquirer');
-// const fs = require('fs');
-const questions = require('./lib/questions.js');
-// const fileName = "./new/logo.svg";
+const Questions = require('./lib/questions.js');
 
-function init() {
-    console.log('Welcome to the simple logo maker!')
-    inquirer 
-    .prompt(questions)
-    .catch(err => {
-            console.log(err)
-        });
-}
-
-init();
+new Questions().run();
