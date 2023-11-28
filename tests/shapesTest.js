39f9e651d154f36edea8a03b5f712986b5dfa3d9
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2023-11-28 14:10:19 -0600

    reverted some previous changes and added testing suite

--- tests/shapesTest.js
+++
@@ -1,9 +0,0 @@
-const {Circle, Triangle, Square} = require('../lib/shapes.js');
-
-describe('Circle class', () => {
-    it('making sure the color is set properly', () => {
-        const color = 'aqua';
-        const circle = new Circle(color);
-        expect(circle.color).toEqual('aqua');
-    })
-})
